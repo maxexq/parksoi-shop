c4acec68c11cf9088b38da0abff60160aa2b32d1
Parames A <parames.ajh@axonstech.com>
2024-02-19 15:28:02 +0700

    feat: 🎸 initial live reload, env config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maxexq/parksoi-shop
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/maxexq/parksoi-shop/config"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+	fmt.Println(cfg.App())
+}
